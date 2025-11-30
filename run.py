39dbd2b4fa9909836c050dedf9b5760724f5f504
danlotas <57368794+danlotas@users.noreply.github.com>
2025-11-30 16:00:36 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,2 @@
-import subprocess
-import sys
 import os
-
-file = os.path.join(os.path.dirname(__file__), "app.py")
-subprocess.run([sys.executable, "-m", "streamlit", "run", file])
+os.system("streamlit run app.py")
